601677267b22b9f6a3fdea1d2c575f2168346fb4
Vũ Đạt <150220982+dathuynh2003@users.noreply.github.com>
2025-06-22 20:24:43 +0700

    [ADD] DI Usecase and Repo

--- OrderFood_BE.WebAPI/OrderFood_BE.Application/Extensions/DependencyInjection.cs
+++ OrderFood_BE.WebAPI/OrderFood_BE.Application/Extensions/DependencyInjection.cs
@@ -5,7 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using OrderFood_BE.Application.UseCase.Implementations.Auth;
+using OrderFood_BE.Application.UseCase.Implementations.Shop;
+using OrderFood_BE.Application.UseCase.Implementations.User;
 using OrderFood_BE.Application.UseCase.Interfaces.Auth;
+using OrderFood_BE.Application.UseCase.Interfaces.Shop;
+using OrderFood_BE.Application.UseCase.Interfaces.User;
 
 namespace OrderFood_BE.Application.Extensions
 {
@@ -14,6 +18,9 @@ namespace OrderFood_BE.Application.Extensions
         public static IServiceCollection AddUseCases(this IServiceCollection services)
         {
             _ = services.AddScoped<IAuthenticationUseCase, AuthenticationUseCase>();
+            _ = services.AddScoped<IUserUseCase, UserUseCase>();
+            _ = services.AddScoped<IShopUseCase, ShopUseCase>();
+
 
             return services;
         }
