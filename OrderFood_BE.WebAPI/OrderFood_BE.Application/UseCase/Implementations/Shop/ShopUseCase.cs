a6b7936aef799c0e56b9546b6b66a465d90ecc5b
Vũ Đạt <150220982+dathuynh2003@users.noreply.github.com>
2025-06-22 20:21:50 +0700

    [ADD] Api Send request create shop

---
+++ OrderFood_BE.WebAPI/OrderFood_BE.Application/UseCase/Implementations/Shop/ShopUseCase.cs
@@ -0,0 +1,41 @@
+﻿using OrderFood_BE.Application.Models.Requests.Shop;
+using OrderFood_BE.Application.Repositories;
+using OrderFood_BE.Application.UseCase.Interfaces.Shop;
+using OrderFood_BE.Shared.Enums;
+
+namespace OrderFood_BE.Application.UseCase.Implementations.Shop
+{
+    public class ShopUseCase : IShopUseCase
+    {
+        private readonly IShopRepository _shopRepository;
+        private readonly IUserRepository _userRepository;
+        public ShopUseCase(IShopRepository shopRepository, IUserRepository userRepository)
+        {
+            _shopRepository = shopRepository;
+            _userRepository = userRepository;
+        }
+        public async Task<string> CreateShopAsync(CreateShopRequest request)
+        {
+            var user = await _userRepository.GetByIdAsync(request.OwnerId);
+            if (user == null || user.Role.Name != RoleEnum.ShopOwner.ToString())
+            {
+                return "User is not authorized to create a shop.";
+            }
+            var shop = new Domain.Entities.Shop
+            {
+                Name = request.Name,
+                //ImageUrl = request.ImageUrl,
+                Address = request.Address,
+                IsActive = false, // Pending approval
+                OpenHours = request.OpenHours,
+                EndHours = request.EndHours,
+                Rating = 0, // Default rating
+                OwnerId = request.OwnerId
+            };
+
+            await _shopRepository.AddAsync(shop);
+            await _shopRepository.SaveChangesAsync();
+            return "The request has been submitted and is awaiting approval.";
+        }
+    }
+}
---
+++ OrderFood_BE.WebAPI/OrderFood_BE.Application/UseCase/Interfaces/Shop/IShopUseCase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderFood_BE.Application.Models.Requests.Shop;
+
+namespace OrderFood_BE.Application.UseCase.Interfaces.Shop
+{
+    public interface IShopUseCase
+    {
+        /// <summary>
+        /// Creates a new shop request for the specified owner.
+        /// </summary>
+        /// <param name="request">The request containing shop details and owner information.</param>
+        /// <returns>
+        /// A message indicating the result of the operation:
+        /// - "User is not authorized to create a shop." if the user is not a shop owner.
+        /// - "The request has been submitted and is awaiting approval." if the shop creation request is successful.
+        /// </returns>
+        Task<string> CreateShopAsync(CreateShopRequest request);
+    }
+}
