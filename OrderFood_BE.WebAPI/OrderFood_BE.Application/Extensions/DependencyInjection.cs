4e52845147edd206b5c22f62734f4e13ab1d940a
PhuocLoc <phuocloc301203@gmail.com>
2025-07-07 00:26:02 +0700

    [ADD] CRUD menu item

--- OrderFood_BE.WebAPI/OrderFood_BE.Application/Extensions/DependencyInjection.cs
+++ OrderFood_BE.WebAPI/OrderFood_BE.Application/Extensions/DependencyInjection.cs
@@ -1,15 +1,17 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using OrderFood_BE.Application.UseCase.Implementations.Auth;
+using OrderFood_BE.Application.UseCase.Implementations.MenuItem;
+using OrderFood_BE.Application.UseCase.Implementations.Shop;
+using OrderFood_BE.Application.UseCase.Implementations.User;
+using OrderFood_BE.Application.UseCase.Interfaces.Auth;
+using OrderFood_BE.Application.UseCase.Interfaces.MenuItem;
+using OrderFood_BE.Application.UseCase.Interfaces.Shop;
+using OrderFood_BE.Application.UseCase.Interfaces.User;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
-using OrderFood_BE.Application.UseCase.Implementations.Auth;
-using OrderFood_BE.Application.UseCase.Implementations.Shop;
-using OrderFood_BE.Application.UseCase.Implementations.User;
-using OrderFood_BE.Application.UseCase.Interfaces.Auth;
-using OrderFood_BE.Application.UseCase.Interfaces.Shop;
-using OrderFood_BE.Application.UseCase.Interfaces.User;
 
 namespace OrderFood_BE.Application.Extensions
 {
@@ -20,7 +22,7 @@ namespace OrderFood_BE.Application.Extensions
             _ = services.AddScoped<IAuthenticationUseCase, AuthenticationUseCase>();
             _ = services.AddScoped<IUserUseCase, UserUseCase>();
             _ = services.AddScoped<IShopUseCase, ShopUseCase>();
-
+            _ = services.AddScoped<IMenuItemUseCase, MenuItemUseCase>();
 
             return services;
         }
---
+++ OrderFood_BE.WebAPI/OrderFood_BE.Application/Repositories/IMenuItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using OrderFood_BE.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderFood_BE.Application.Repositories
+{
+    public interface IMenuItemRepository : IGenericRepository<MenuItem, Guid>
+    {
+        Task<IEnumerable<MenuItem>?> GetMenuItemByShopIdAsync(Guid id);
+        Task<IEnumerable<MenuItem>?> GetAllMenuItemsAsync();
+
+    }
+}
